ae372f5050894f64daf7dd32b9be2ec2a997df0a
Inacio Cerqueira <inaciogcerqueira@gmail.com>
2020-02-10 13:21:40 -0300

    Updates

---
+++ precache-manifest.f1671fb1fb0ac5a503368235255f7657.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be424eb181f6d320c2c076b00d834ecb",
+    "url": "/my-world-map/index.html"
+  },
+  {
+    "revision": "e524c884b172cc42092d",
+    "url": "/my-world-map/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6273b2754081e06aaa3c",
+    "url": "/my-world-map/static/js/2.872de2d6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-world-map/static/js/2.872de2d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e524c884b172cc42092d",
+    "url": "/my-world-map/static/js/main.b316ecc9.chunk.js"
+  },
+  {
+    "revision": "e5147dbde30e7714a13c",
+    "url": "/my-world-map/static/js/runtime-main.ef641298.js"
+  }
+]);
